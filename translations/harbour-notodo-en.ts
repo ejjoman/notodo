298a81811581cafb2538e158f02bf0d9df5cee55
Michael Neufing <michael@neufing.org>
2015-03-08 21:25:00 +0100

    Marked all english translations as finished

--- translations/harbour-notodo-en.ts
+++ translations/harbour-notodo-en.ts
@@ -1,39 +1,39 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.0" language="en" sourcelanguage="en">
 <context>
     <name>AboutModel</name>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="9"/>
         <source>Project Home</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="10"/>
         <source>on GitHub</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="11"/>
         <location filename="../qml/models/AboutModel.qml" line="17"/>
         <location filename="../qml/models/AboutModel.qml" line="23"/>
         <source>Project</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="15"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="21"/>
         <source>Report a bug or send a feature request</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="22"/>
         <source>on GitHub Issues</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="27"/>
@@ -42,43 +42,43 @@
         <location filename="../qml/models/AboutModel.qml" line="44"/>
         <location filename="../qml/models/AboutModel.qml" line="50"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="36"/>
         <source>Twitter</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="42"/>
         <location filename="../qml/models/AboutModel.qml" line="48"/>
         <source>Donate</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="43"/>
         <source>via PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="49"/>
         <source>via Flattr</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="54"/>
         <source>Translate this app</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="55"/>
         <source>on translate.neufing.org</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/models/AboutModel.qml" line="56"/>
         <source>Translation</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -86,17 +86,17 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="18"/>
         <source>About NoTodo</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="38"/>
         <source>Link will open</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="41"/>
         <source>Mail app will open</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -104,7 +104,7 @@
     <message>
         <location filename="../qml/pages/LicensePage.qml" line="21"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -112,52 +112,52 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="50"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="56"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="60"/>
         <source>New note</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="65"/>
         <source>New task list</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="147"/>
         <source>All done</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="149"/>
         <source>%1 of %2 done</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="162"/>
         <source>Deleting</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="172"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="188"/>
         <source>No notes or task lists available yet</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="189"/>
         <source>Add a new note or task list</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -165,12 +165,12 @@
     <message>
         <location filename="../qml/pages/NotePage.qml" line="42"/>
         <source>New note</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/NotePage.qml" line="104"/>
         <source>Write a new note</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -178,24 +178,24 @@
     <message>
         <location filename="../qml/common/NotePagePullDownMenu.qml" line="32"/>
         <source>Change to &lt;b&gt;task list&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/common/NotePagePullDownMenu.qml" line="32"/>
         <source>Change to &lt;b&gt;note&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message numerus="yes">
         <location filename="../qml/common/NotePagePullDownMenu.qml" line="64"/>
         <source>%n words</source>
-        <translation type="unfinished">
+        <translation>
             <numerusform></numerusform>
         </translation>
     </message>
     <message>
         <location filename="../qml/common/NotePagePullDownMenu.qml" line="74"/>
         <source>%1 of %2 (%3%) done</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -203,13 +203,13 @@
     <message>
         <location filename="../qml/pages/TodoPage.qml" line="42"/>
         <source>New task list</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../qml/pages/TodoPage.qml" line="133"/>
         <location filename="../qml/pages/TodoPage.qml" line="134"/>
         <source>New entry</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
